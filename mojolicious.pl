c46ef2ab474d8ea0613c56b1e7e13f94dcbd0a4f
Sebastian Riedel <sri@cpan.org>
2011-06-08 22:10:40 +0200

    small formatting tweak

--- mojolicious.pl
+++ mojolicious.pl
@@ -230,7 +230,7 @@ __DATA__
     $example-&gt;post(&#39;/title&#39;)-&gt;to(&#39;#title&#39;);
 
     # ... and much, much more
-    # including multiple, auto-discovered controllers
+    # (including multiple, auto-discovered controllers)
     $r-&gt;websocket(&#39;/echo&#39;)-&gt;to(&#39;realtime#echo&#39;);
   }
 
