9791284db8c9e2df92716b8774ee8a53503df506
Sebastian Riedel <sri@cpan.org>
2014-05-29 22:58:15 +0200

    a new Perl has been released

--- mojolicious.pl
+++ mojolicious.pl
@@ -96,7 +96,7 @@ curl -L cpanmin.us | perl - -n  Mojolicious
       </li>
       <li>
         Very clean, portable and Object Oriented pure-Perl API without any
-        hidden magic and no requirements besides Perl 5.10.1 (although 5.16+
+        hidden magic and no requirements besides Perl 5.10.1 (although 5.18+
         is recommended, and optional CPAN modules will be used to provide
         advanced functionality if they are installed).
       </li>
