07fee6854f9a85bc84dddf5e8c0752b968d28702
Sebastian Riedel <sri@cpan.org>
2014-08-26 00:52:27 +0200

    slightly better feature description

--- mojolicious.pl
+++ mojolicious.pl
@@ -90,10 +90,10 @@ curl -L cpanmin.us | perl - -n  Mojolicious
         </blockquote>
       </li>
       <li>
-        Very clean, portable and Object Oriented pure-Perl API without any
-        hidden magic and no requirements besides Perl 5.18.0 (versions as old
-        as 5.10.1 can be used too, but may require additional CPAN modules to
-        be installed)
+        Very clean, portable and Object Oriented pure-Perl API with no hidden
+        magic and no requirements besides Perl 5.18.0 (versions as old as
+        5.10.1 can be used too, but may require additional CPAN modules to be
+        installed)
       </li>
       <li>
         Full stack HTTP and WebSocket client/server implementation with IPv6,
